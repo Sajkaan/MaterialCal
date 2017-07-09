ebddc0df62dd020dc560f7a4f7ae22c726a78f94
sajkaan <safet.garic@hotmail.com>
2017-07-09 21:28:00 +0200

    Add repository package

---
+++ src/main/java/com/sajkaan/repository/PartRepository.java
@@ -0,0 +1,7 @@
+package com.sajkaan.repository;
+
+import com.sajkaan.model.Part;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PartRepository extends CrudRepository<Part, Long> {
+}
---
+++ src/main/java/com/sajkaan/repository/TrailerRepository.java
@@ -0,0 +1,7 @@
+package com.sajkaan.repository;
+
+import com.sajkaan.model.Trailer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TrailerRepository extends CrudRepository<Trailer, Long> {
+}
