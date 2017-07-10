0a97d545a8d5df15b1cb1db9dbc1e739bdf41875
sajkaan <safet.garic@hotmail.com>
2017-07-10 21:45:00 +0200

    Insert html pages and styles.css

--- src/main/java/com/sajkaan/controller/NewTrailer.java
+++ src/main/java/com/sajkaan/controller/NewTrailer.java
@@ -1,8 +1,25 @@
 package com.sajkaan.controller;
 
+import com.sajkaan.service.PartService;
+import com.sajkaan.service.TrailerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class NewTrailer {
+
+    @Autowired
+    private TrailerService trailerService;
+
+    @Autowired
+    private PartService partService;
+
+    @RequestMapping("/newTrailer")
+    public String newTrailerAdding(Model model){
+
+        return "new";
+    }
 
 }
