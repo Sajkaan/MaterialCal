78a5c6ffe400eaf08255f0e729956b1c17ab97b3
sajkaan <safet.garic@hotmail.com>
2017-07-15 00:24:10 +0200

    Add to app.js option to add text input to cell

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,8 +1,14 @@
 
+   function onInput() {
+        var partName = document.getElementById("partName").value;
+   }
+
+
    function newRow() {
           var table = document.getElementById("tab");
           var row = table.insertRow(1);
           for (var i = 0; i < 7; i++) {
             var cell1 = row.insertCell(i);
+            cell1.innerHTML = partName.value;
           }
       }
