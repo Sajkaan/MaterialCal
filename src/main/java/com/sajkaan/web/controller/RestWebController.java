2196af8b4ac3da3336a73b934289a2ccbec6dcfe
sajkaan <safet.garic@hotmail.com>
2017-07-18 17:13:14 +0200

    Add get request "getAllParts" in RestWebController

--- src/main/java/com/sajkaan/web/controller/RestWebController.java
+++ src/main/java/com/sajkaan/web/controller/RestWebController.java
@@ -1,6 +1,25 @@
 package com.sajkaan.web.controller;
 
+import com.sajkaan.message.Response;
+import com.sajkaan.service.PartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RestWebController {
+
+    @Autowired
+    private PartService parts;
+
+    @RequestMapping(value = "/getAllParts", method = RequestMethod.GET)
+    public Response getResource() {
+        Response response = new Response("Done", parts);
+
+        return response;
+    }
+
 
 
 }
