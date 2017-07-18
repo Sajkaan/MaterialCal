dd6889edcf4e29e08366e35fc7fe4cbc2d4151ad
sajkaan <safet.garic@hotmail.com>
2017-07-18 17:16:16 +0200

    Add post request "newTrailer/postParts" in RestWebController

--- src/main/java/com/sajkaan/web/controller/RestWebController.java
+++ src/main/java/com/sajkaan/web/controller/RestWebController.java
@@ -1,8 +1,10 @@
 package com.sajkaan.web.controller;
 
 import com.sajkaan.message.Response;
+import com.sajkaan.model.Part;
 import com.sajkaan.service.PartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,14 @@ public class RestWebController {
     @RequestMapping(value = "/getAllParts", method = RequestMethod.GET)
     public Response getResource() {
         Response response = new Response("Done", parts);
+
+        return response;
+    }
+
+    @RequestMapping(value = "newTrailer/postParts", method = RequestMethod.POST)
+    public Response postPart(@RequestBody Part part) {
+        parts.save(part);
+        Response response = new Response("Done", part);
 
         return response;
     }
