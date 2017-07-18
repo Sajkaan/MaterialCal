3428fe30f261ffad23fb2dd72039a487b488a184
sajkaan <safet.garic@hotmail.com>
2017-07-18 16:51:49 +0200

    Add String and Object attribute in Response.class with getters and setters

--- src/main/java/com/sajkaan/message/Response.java
+++ src/main/java/com/sajkaan/message/Response.java
@@ -1,4 +1,30 @@
 package com.sajkaan.message;
 
 public class Response {
+    private String status;
+    private Object object;
+
+    public Response() {
+    }
+
+    public Response(String status, Object object) {
+        this.status = status;
+        this.object = object;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
 }
