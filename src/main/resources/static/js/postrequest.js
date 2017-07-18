fe6d1ceb40e474d87c459427c89a4b8089c43673
sajkaan <safet.garic@hotmail.com>
2017-07-18 16:49:32 +0200

    Add RestWebController

--- src/main/resources/static/js/postrequest.js
+++ src/main/resources/static/js/postrequest.js
@@ -1,52 +1,52 @@
-$( document ).ready(function() {
+  $( document ).ready(function() {
 
-	var url = window.location;
+  	var url = window.location;
 
-	// SUBMIT FORM
-    $("#elementsInput").submit (function (event){
-        event.preventDefault();
-        ajaxPost();
-        });
+  	// SUBMIT FORM
+      $("#elementsInput").submit (function (event){
+          event.preventDefault();
+          ajaxPost();
+          });
 
-    function ajaxPost(){
+      function ajaxPost(){
 
-        // PREP FORM DATA
-        var formData = {
-            partName : $("partName").val(),
-            material : $("material").val(),
-            netoWeight : $("netoWeight").val() / 1000,
-            height : $("height").val(),
-            leng : $("leng").val(),
-            thickness : $("thickness").val()
-            brutoWeight : height * leng;
-        }
+          // PREP FORM DATA
+          var formData = {
+              partName : $("partName").val(),
+              material : $("material").val(),
+              netoWeight : $("netoWeight").val() / 1000,
+              height : $("height").val(),
+              leng : $("leng").val(),
+              thickness : $("thickness").val()
+              brutoWeight : height * leng;
+          }
 
-    	// DO POST
-    	$.ajax({
-			type : "POST",
-			contentType : "application/json",
-			url : url + "/postParts",
-			data : JSON.stringify(formData),
-			dataType : 'json',
-			success : function(result) {
-                    console.log(result);
-			},
-			error : function(e) {
-				console.log("ERROR: ", e);
-			}
-		});
+      	// DO POST
+      	$.ajax({
+  			type : "POST",
+  			contentType : "application/json",
+  			url : url + "/postParts",
+  			data : JSON.stringify(formData),
+  			dataType : 'json',
+  			success : function(result) {
+                      console.log(result);
+  			},
+  			error : function(e) {
+  				console.log("ERROR: ", e);
+  			}
+  		});
 
-    	// Reset FormData after Posting
-    	resetData();
+      	// Reset FormData after Posting
+      	resetData();
 
-    }
+      }
 
-    function resetData(){
-            $("partName").val(""),
-            $("material").val(""),
-            $("netoWeight").val(""),
-            $("height").val(""),
-            $("leng").val(""),
-            $("thickness").val("")
-    }
-})
+      function resetData(){
+              $("partName").val(""),
+              $("material").val(""),
+              $("netoWeight").val(""),
+              $("height").val(""),
+              $("leng").val(""),
+              $("thickness").val("")
+      }
+  })
