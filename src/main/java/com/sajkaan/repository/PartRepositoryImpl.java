0311a4ac4d5573f0b924239ce1658d3fb6bd58de
sajkaan <safet.garic@hotmail.com>
2017-07-19 20:36:01 +0200

    Add PartRepositoryImpl method findAll, findById, delete

--- src/main/java/com/sajkaan/repository/PartRepositoryImpl.java
+++ src/main/java/com/sajkaan/repository/PartRepositoryImpl.java
@@ -6,6 +6,10 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
+
 
 @Repository
 public class PartRepositoryImpl implements PartRepository {
@@ -15,12 +19,54 @@ public class PartRepositoryImpl implements PartRepository {
 
 
     @Override
+    public List<Part> findAll() {
+
+        Session session = sessionFactory.openSession();
+
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+
+        CriteriaQuery<Part> criteriaQuery = criteriaBuilder.createQuery(Part.class);
+
+        criteriaQuery.from(Part.class);
+
+        List<Part> parts = session.createQuery(criteriaQuery).getResultList();
+
+        session.close();
+
+        return parts;
+    }
+
+    @Override
+    public Part findById(Long id) {
+        Session session = sessionFactory.openSession();
+
+        Part part = session.get(Part.class, id);
+
+        session.close();
+
+        return part;
+    }
+
+    @Override
     public void save(Part part) {
         Session session = sessionFactory.openSession();
 
         session.beginTransaction();
 
         session.saveOrUpdate(part);
+
+        session.getTransaction().commit();
+
+        session.close();
+    }
+
+    @Override
+    public void delete(Part part) {
+        Session session = sessionFactory.openSession();
+
+        session.beginTransaction();
+
+        session.delete(part);
 
         session.getTransaction().commit();
 
