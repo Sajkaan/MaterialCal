d304231c10d6480301865b3769170f1ba11c55ed
sajkaan <safet.garic@hotmail.com>
2017-07-24 00:06:22 +0200

    Change name of NewTrailer to MainWebController

--- src/main/java/com/sajkaan/web/controller/MainWebController.java
+++ src/main/java/com/sajkaan/web/controller/MainWebController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-public class NewTrailer {
+public class MainWebController {
 
     @Autowired
     private TrailerService trailerService;
